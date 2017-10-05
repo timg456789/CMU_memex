f2099f8b4f46c22e7b4423f916738a8302209151
Timothy Gonzalez <timg456789@yahoo.com>
2017-10-05 00:15:52 -0400

    Make input and output files configurable.

--- Eclipse/Memex/src/memex/TJBatchExtractor.java
+++ Eclipse/Memex/src/memex/TJBatchExtractor.java
@@ -9,21 +9,24 @@ class TJBatchExtractor {
 
 	public static void main(String[] args) throws Exception {
 		
-		InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream("Example_text.txt"), "UTF-8");
+		String inputFileName = args[0];
+		String outputFileName = args[1];
+		
+		InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(inputFileName), "UTF-8");
 		BufferedReader bufferedFileReader = new BufferedReader(inputStreamReader);
 		
 		BackpageAdParser backpageAdParser = new BackpageAdParser();
 		GateOutputModel result = backpageAdParser.Parse(
 				Runtime.getRuntime().availableProcessors(), bufferedFileReader);
 		
-		PrintWriter writer = new PrintWriter("Out.csv","UTF-8");
+		PrintWriter writer = new PrintWriter(outputFileName + ".csv","UTF-8");
 		writer.println("Perspective_1st,Perspective_3rd,Name,Age,Cost,Height_ft,Height_in,Weight,Cup,Chest,Waist,Hip,Ethnicity,SkinColor,EyeColor,HairColor,Restriction_Type,Restriction_Ethnicity,Restriction_Age,PhoneNumber,AreaCode_State,AreaCode_Cities,Email,Url,Media");
 		for(String l : result.results) {
 			writer.println(l);
 		}
 		writer.close();
 
-		PrintWriter writer2 = new PrintWriter("Out.txt","UTF-8");
+		PrintWriter writer2 = new PrintWriter(outputFileName + ".txt","UTF-8");
 		for(String l : result.text) {
 			writer2.println(l);
 		}
